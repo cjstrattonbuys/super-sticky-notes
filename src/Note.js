30e3ff0e3f80148f05121a4f8e21036de46a4b81
CJ Buys <cjstrattonbuys@gmail.com>
2022-03-19 13:50:42 -0400

    Render from the props and state

--- src/Note.js
+++ src/Note.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Note = (props) => (
   <li className="note">
-    <input className="note__title" type="text" placeholder="Title" />
-    <textarea className="note__description" placeholder="Description..." />
+    <input className="note__title" type="text" placeholder="Title" value={props.note.title} />
+    <textarea className="note__description" placeholder="Description..." value={props.note.description} />
     <span className="note__delete">X</span>
   </li>
 );
--- src/NotesList.js
+++ src/NotesList.js
@@ -2,9 +2,12 @@ import React from "react";
 import Note from "./Note.js";
 
 const NotesList = (props) => {
-    const renderNote = (note) => <Note note={note} key={note.id} />;
-    const noteElements = props.notes.map(renderNote);
-    return <ul className="notes-list">{noteElements}</ul>;
+  const keepSearchMatches = (note) => note.doesMatchSearch;
+  const searchMatches = props.notes.filter(keepSearchMatches);
+
+  const renderNote = (note) => <Note note={note} key={note.id} />;
+  const noteElements = props.notes.map(renderNote);
+  return <ul className="notes-list">{noteElements}</ul>;
 };
 
 export default NotesList;
